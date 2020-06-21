5e95fc3f3b4f486b6ad3aa8b40d819adaf497b0d
csviri <csviri@gmail.com>
2020-06-21 12:52:31 +0200

    syntax fix

--- src/main/java/com/coderealms/gitopsactions/deploy/DeployApplication.java
+++ src/main/java/com/coderealms/gitopsactions/deploy/DeployApplication.java
@@ -18,6 +18,8 @@ public class DeployApplication implements CommandLineRunner {
     @Override
     public void run(String... args) {
         System.out.println(Arrays.toString(args));
+
+
     }
 
     @Bean
